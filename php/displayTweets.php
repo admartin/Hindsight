55de608e39f7d0944b3f790c76085fe07261f8f3
Michael Kovalsky <kovalsky@uga.edu>
2017-04-11 19:19:27 -0400

    Commiting php code which can fetch data from a specific user

--- php/displayTweets.php
+++ php/displayTweets.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once ("twitter_php_interface/TwitterAPIExchange.php");
 session_start();
 
 /**
@@ -9,51 +10,84 @@ session_start();
  * Time: 7:23 PM
  */
 
+$credentials = array(
+    'oauth_access_token' => "847109906826186752-IqDb0ueSTWvNpx7PH24h8w9FrKOCJza",
+    'oauth_access_token_secret' => "fInPOqtpCNd5wVONyQPHofxFepUbMuir1s1kWib8FUFIu",
+    'consumer_key' => "jyIwjRE1QwU4yAFCuJRF1W2ND",
+    'consumer_secret' => "lhtxvG8TYIy1DrxjLMkU22EqVU9tK5Zn2y3s521Y91m7HH3kHn"
+);
 
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$requestMethod = "GET";
 
 
 if(isset($_SESSION['username'])) {
 
+    $query = '?screen_name=' . $_SESSION['username'] . '&count=100';
 
+    $twitter = new TwitterAPIExchange($credentials);
+    $result = json_decode($twitter->setGetfield($query)
+        ->buildOauth($url, $requestMethod)
+        ->performRequest(), $assoc = TRUE);
 
-        $servername = "localhost:3306"; //modify this with your own server
-        $dbusername = "root"; //Modify this with your own username
-        $dbpassword = "Jaljap2732!"; //modify this with your own password
+    if($result["error"][0]["message"] != "") {
+        echo "Error With Twiiter API Exchange";
+    } else {
 
-        try {
+        $htmlBody = ""; // an empty html body response for now.
+        $tweetsParagraph = "";
+        $tweet = "";
 
-            $connection = new PDO("mysql:host=$servername;dbname=rawdata",
-                $dbusername,
-                $dbpassword
-            );
+        foreach ($result as $item) {
+            $tweet = $item['text'];
+            $tweetsParagraph .= "<p>" ."$tweet</p>";
+        }
 
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $query = $connection->prepare("SELECT t.tweet FROM tweet t WHERE
-                t.User_username = ?");
-
-            $query->execute(array($_SESSION['username']));
-
-            $htmlBody = ""; // an empty html body response for now.
-            $tweetsParagraph = "";
-            $tweet = "";
-
-            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-
-                $tweet = $row["tweet"];
-                $tweetsParagraph .= "<p>Tweet: $tweet</p>";
-
-            }
-
-            $htmlBody = <<<END
+        $htmlBody = <<<END
                 $tweetsParagraph;
 END;
 
-            echo $htmlBody;
+        echo $htmlBody;
+    }
 
-        } catch (PDOException $e) {
-            echo "Connection Failed With Error: " .$e->getMessage();
-        }
+//        $servername = "localhost:3306"; //modify this with your own server
+//        $dbusername = "root"; //Modify this with your own username
+//        $dbpassword = "Jaljap2732!"; //modify this with your own password
+//
+//        try {
+//
+//            $connection = new PDO("mysql:host=$servername;dbname=rawdata",
+//                $dbusername,
+//                $dbpassword
+//            );
+//
+//            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//
+//            $query = $connection->prepare("SELECT t.tweet FROM tweet t WHERE
+//                t.User_username = ?");
+//
+//            $query->execute(array($_SESSION['username']));
+//
+//            $htmlBody = ""; // an empty html body response for now.
+//            $tweetsParagraph = "";
+//            $tweet = "";
+//
+//            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+//
+//                $tweet = $row["tweet"];
+//                $tweetsParagraph .= "<p>Tweet: $tweet</p>";
+//
+//            }
+//
+//            $htmlBody = <<<END
+//                $tweetsParagraph;
+//END;
+//
+//            echo $htmlBody;
+//
+//        } catch (PDOException $e) {
+//            echo "Connection Failed With Error: " .$e->getMessage();
+//        }
 
     } else {
         echo "Error with session variable, username not set!";
