c061f5d867f3fe22bdd3c43a9c5450f1a0bd5bfa
Michael Kovalsky <kovalsky@uga.edu>
2017-04-11 23:38:17 -0400

    messed around some with the code, added the basic graph feature just as a stand in for now. Now, we will have to mess around with the CSS.

--- javascript/displayGraph.js
+++ javascript/displayGraph.js
@@ -4,4 +4,31 @@
 
 function displayGraph() {
     alert("Work in Progress");
+
+    $('#graphResult').show();
+
+    $('#graphResult').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }],
+    });
+
 }
